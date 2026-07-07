3b04b815ba8b368a02359b9205d67d5f5c1b8940
Zvkyyy-gif <kemaenwonk@gmail.com>
2026-07-07 21:55:31 +0700

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,5 +1,8 @@
 export default async function handler(req, res) {
-
+  
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET");
+  
   if (req.method !== "GET") {
     return res.status(405).json({
       success: false,
